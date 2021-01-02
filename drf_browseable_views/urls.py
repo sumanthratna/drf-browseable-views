9fc4c22498e14fb912092c2f72c3f3f3ecc00a0f
Sumanth Ratna <2022sratna@tjhsst.edu>
2021-01-01 19:44:19 -0500

    Put REST Framework views under 'drf' application namespace

--- drf_browseable_views/urls.py
+++ drf_browseable_views/urls.py
@@ -14,17 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MockView, mock_view, MockViewSet
 
 # in this demo, we don't register /admin/ in the router to show how you can
 # exclude a route from appearing in the browseable API. the browseable API will
 # only show views that are registered in the router.
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
 
 django_view_router = DefaultRouter()
 # TODO: django-class2 and drf-viewset do not work
@@ -34,7 +30,12 @@ django_view_router.register(
 django_view_router.register('django-class2', MockView, basename='example1.2')
 django_view_router.register('django-function', mock_view, basename='example2')
 django_view_router.register('drf-viewset', MockViewSet, basename='example3')
-# the browseable API should be at / and should display the above three routes
+# the browseable API should be at / and should display the above four routes
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', include(django_view_router.urls)),
+    path('', include((django_view_router.urls, 'drf'))),  # application namespace
+]
 
 urlpatterns += django_view_router.urls
